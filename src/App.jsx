36be9aeb0c61378524c9da5832e324a5dd4e5ad7
feature-A <vyijardar@YuVi>
2024-11-20 14:26:37 -0600

    Updating all code

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AllPlayers />} />
         <Route path="/:id" element={<SinglePlayer />} />
+        <Route path="/add-player" element={<AddNewPlayer />} />
       </Routes>
     </>
   );
